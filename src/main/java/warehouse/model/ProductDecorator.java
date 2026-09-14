c56f97017ae67ae6c51603d9004cdfc33d4c384f
Anders Eriksson <anteman.swe@gmail.com>
2026-09-14 21:41:14 +0200

    Introduce `ProductDecorator` abstract class to enable flexible decoration of `Sellable` items

---
+++ src/main/java/warehouse/model/ProductDecorator.java
@@ -0,0 +1,37 @@
+package warehouse.model;
+
+
+import java.math.BigDecimal;
+
+public abstract class ProductDecorator implements Sellable {
+    private final Sellable decoratedProduct;
+
+    protected ProductDecorator(Sellable decoratedProduct) {
+        this.decoratedProduct = decoratedProduct;
+    }
+
+    @Override
+    public String getId() {
+        return decoratedProduct.getId();
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return decoratedProduct.getPrice();
+    }
+
+    @Override
+    public String getName() {
+        return decoratedProduct.getName();
+    }
+
+    @Override
+    public Category getCategory() {
+        return decoratedProduct.getCategory();
+    }
+
+    @Override
+    public int getQuantity() {
+        return decoratedProduct.getQuantity();
+    }
+}
